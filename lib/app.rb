8b3321e8be365e3dfc6eeddf5220a56329aea11c
Robert Faldo <robertfaldo@gmail.com>
2018-05-30 11:08:33 +0100

    compelted returning HTML challenge, added a /cat route and got the server to respond with a picture of a cat

--- lib/app.rb
+++ lib/app.rb
@@ -1,13 +1,20 @@
 require 'sinatra'
+set :session_secret, 'super secret'
 
-get "/" do 
+get "/" do
   "hello"
 end
 
-get "/secret" do 
+get "/secret" do
   "this is a secret message"
 end
 
-get "/test" do 
+get "/test" do
   "this prints without updating server"
+end
+
+get "/cat" do
+  "<div style='border: 3px dashed red'>
+    <img src='http://f.cl.ly/items/0k0v3e2X3l2f3i1n1Y19/Screen%20Shot%202013-09-10%20at%2011.32.00.png'>
+  </div>"
 end
