5b823c424d3b550fe2fa0206fc42cb6163099eb6
Robert Faldo <robertfaldo@gmail.com>
2018-05-30 13:08:37 +0100

    Compelted Sinatra Introducing params

--- app.rb
+++ app.rb
@@ -13,11 +13,16 @@ get "/test" do
   "this prints without updating server"
 end
 
-get "/cat" do
+get "/random-cat" do
   @name = ["Amigo", "Oscar", "Viking"].sample
   erb(:index)
 end
 
 get "/testing" do
   erb "Hi there <%=2 + 2%>"
+end
+
+get "/named-cat" do
+  @name = params[:name]
+  erb(:index)
 end
